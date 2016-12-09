2c671c60ef596054ffde00745162e31d73c48adf
Gunner <Gunner>
2016-12-09 23:58:11 +0300

    cells-rotate add some comments

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
+//обработчик кнопки Start
 var startButton = document.querySelector('.btn-start');
 startButton.onclick = function () {
   console.log('PROGRESS START');
   startButton.innerText = "In progress";
   startButton.classList.add('in-progress');
+  //в массив
   var cellList = [].slice.call(document.getElementsByClassName('cell'));
   var cellCount = cellList.length;
 
+  //перебор всех ячеек
   cellList.forEach(function(cell, i) {
+    //запуск эффекта поворотас определенной задержкой для каждой из ячеек
     setTimeout(function() {
       console.log("Cell " + ++i + " animation start");
       cell.classList.add('rotate180'); 
       
+      //вешаем обработчик событий на отслеживание конца transition
       cell.addEventListener('transitionend', function(e) {
         console.log("Cell " + e.target.innerText + " animation end");
         if(i == cellCount) {
@@ -24,6 +29,7 @@ startButton.onclick = function () {
   });
 }
 
+//обработчик для модального окна
 document.querySelector('.btn-modal-close').onclick = function () {
   document.querySelector('.modal-window').style.display = "none";
 }
